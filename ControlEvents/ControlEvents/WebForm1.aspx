4add37b29435a715ec2d793168abfc84491df0a2
Issey Jordan <Issey.Jordan@inspectrealestate.com.au>
2021-10-14 10:06:30 +1000

    ASP.NET Server control events Part 7

---
+++ ControlEvents/ControlEvents/WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ControlEvents.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="height: 159px">
+            <br />
+            <br />
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged" AutoPostBack="True"></asp:TextBox>
+            &nbsp;&nbsp;
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="TextBox1" ErrorMessage="RequiredFieldValidator"></asp:RequiredFieldValidator>
+
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ControlEvents/ControlEvents/WebForm1.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ControlEvents
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_PreInit(object sender, EventArgs e)
+        { Response.Write("Page_PreInit" + "<br/>"); }
+
+        protected void Page_Init(object sender, EventArgs e)
+        { Response.Write("Page_Init" + "<br/>"); }
+
+        protected void Page_InitComplete(object sender, EventArgs e)
+        { Response.Write("Page_InitComplete" + "<br/>"); }
+
+        protected void Page_PreLoad(object sender, EventArgs e)
+        { Response.Write("Page_PreLoad" + "<br/>"); }
+
+        protected void Page_Load(object sender, EventArgs e)
+        { Response.Write("Page_Load" + "<br/>"); }
+
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        { Response.Write("Page_LoadComplete" + "<br/>"); }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        { Response.Write("Page_PreRender" + "<br/>"); }
+
+        protected void Page_PreRenderComplete(object sender, EventArgs e)
+        { Response.Write("Page_PreRenderComplete" + "<br/>"); }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            //Response.Write("Page_Unload" + "<br/>"); 
+        }
+
+        //  Postback event, immediately sent to server for processing
+        //  
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("Button Clicked <br />");
+        }
+
+        //  Cached event, cached/saved in the the page's view state (when clicking outside text
+        //  is still there). until a postback occurs they are cached in the view state, the cached events
+        //  and the postback events are executed together
+        //  
+        //  AutoPostBack to automatically post to the server when the text is
+        //  is modified. tab or lose focus
+        //
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Response.Write("Text Changed <br />");
+        }
+
+        //  Validation events occur on the client, the check occurs before postback
+
+
+    }
+}
